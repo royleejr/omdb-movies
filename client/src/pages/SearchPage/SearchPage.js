931ce000282346f53926c44219e79dec1284e0c0
Roy Lee <royk.leejr@gmail.com>
2021-01-14 05:55:16 -0500

    started styling search page for mobile. Created hero image and input.

--- client/src/pages/SearchPage/SearchPage.js
+++ client/src/pages/SearchPage/SearchPage.js
@@ -1,5 +1,24 @@
+import React from "react";
+import { ReactComponent as SearchSvg } from "../../assets/icons/search.svg";
 import "./SearchPage.scss";
 
 export default function SearchPage() {
-  return <div>Search Page</div>;
+  return (
+    <div className="search">
+      <div className="search__hero">
+        <div className="search__input-container">
+          <input
+            className="search__input"
+            type="text"
+            id="title"
+            name="title"
+            placeholder="Search for movie title"
+          ></input>
+          <button className="search__search-button">
+            <SearchSvg className="search__search-icon" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
