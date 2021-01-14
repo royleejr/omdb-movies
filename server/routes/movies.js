36bebbf6a4d5f992897e7513444a0672ef4b8baa
Roy Lee <royk.leejr@gmail.com>
2021-01-14 04:10:20 -0500

    created post and put functionality for movies route with functionality and some validation

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -1,8 +1,40 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
 
-router.get("/", (req, res) => {
-  res.send("Movies route working");
+const nominationsData = require("../data/nominationsData.json");
+
+router.post("/", (req, res) => {
+  //add more validation when data set is more set in stone
+  if (!req.body) {
+    res.send("Please add item to post");
+  } else {
+    const newNominationsData = [...nominationsData, req.body];
+    fs.writeFileSync(
+      "./data/nominationsData.json",
+      JSON.stringify(newNominationsData, null, 2)
+    );
+    res.send("Post Successful").status(200);
+  }
+});
+
+router.put("/", (req, res) => {
+  if (!req.body) {
+    res.send("Please add movie ID to delete");
+  } else {
+    const newNominationsData = nominationsData.filter((item) => {
+      return item.id !== req.body.id;
+    });
+    if (newNominationsData.length === nominationsData.length) {
+      res.send("ID did not match any from the nominations list");
+    } else {
+      fs.writeFileSync(
+        "./data/nominationsData.json",
+        JSON.stringify(newNominationsData, null, 2)
+      );
+      res.send("Post Successful").status(200);
+    }
+  }
 });
 
 module.exports = router;
--- server/routes/search.js
+++ server/routes/search.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
-const { reset } = require("nodemon");
 
 router.get("/:title", (req, res) => {
   axios
