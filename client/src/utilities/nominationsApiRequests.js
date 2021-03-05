65418f5e6edeaea77ef82f98ed7d351acb1be411
Roy Lee <royk.leejr@gmail.com>
2021-03-04 21:19:16 -0500

    changed url to heroku

--- client/src/utilities/nominationsApiRequests.js
+++ client/src/utilities/nominationsApiRequests.js
@@ -1,15 +1,21 @@
 import axios from "axios";
 
 const getNominations = () => {
-  return axios.get("http://localhost:8080/nominations");
+  return axios.get("https://omdb-movie-server.herokuapp.com/nominations");
 };
 
 const addNomination = (movie) => {
-  return axios.post("http://localhost:8080/nominations", movie);
+  return axios.post(
+    "https://omdb-movie-server.herokuapp.com/nominations",
+    movie
+  );
 };
 
 const removeNomination = (movie) => {
-  return axios.put("http://localhost:8080/nominations", movie);
+  return axios.put(
+    "https://omdb-movie-server.herokuapp.com/nominations",
+    movie
+  );
 };
 
 export { getNominations, addNomination, removeNomination };
